3fc5300e61f931527bafd48103988b441d333070
jungnamsik <nsjung74@gmail.com>
2020-04-07 15:27:47 +0900

    commit test

--- helloflask/__init__.py
+++ helloflask/__init__.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, g
+
 
 app = Flask(__name__)
+app.debug = True
+
+@app.before_request
+def before_request() :
+    print ("before_request~!")
+    g.str = "한글"
+
+
 
 @app.route("/")
 def helloworld() :
